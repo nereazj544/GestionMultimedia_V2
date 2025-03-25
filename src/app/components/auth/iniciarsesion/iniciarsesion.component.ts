7a999980c09f38e79f0d80e2f957f50769194429
nereazj544 <nzjvirtual@gmail.com>
2025-03-25 17:23:03 +0100

    Agregar iconos de ojo para mostrar/ocultar contraseña y mejorar el diseño del formulario de inicio de sesión y registro

--- src/app/components/auth/iniciarsesion/iniciarsesion.component.ts
+++ src/app/components/auth/iniciarsesion/iniciarsesion.component.ts
@@ -21,6 +21,8 @@ export class IniciarsesionComponent implements OnDestroy, OnInit {
   imagenes = [
     'https://i.pinimg.com/736x/24/1a/33/241a33e322eade5ed3f487b2ac9ea9f5.jpg',
     'https://i.pinimg.com/236x/f8/1b/39/f81b39d7555259c63c23cca0ac0b635e.jpg',
+    'https://i.pinimg.com/736x/02/aa/6f/02aa6fc8f112bce0f0fcfd47b8940666.jpg',
+    'https://i.pinimg.com/736x/21/e4/ee/21e4eebb78a1febe8e8268a6c24056d8.jpg'
   ];
 
   currentImgLng = this.imagenes[0];
