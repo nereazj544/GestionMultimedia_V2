b504e800f3415f85fb416cb1aba03cfd1d7de3b8
nereazj544 <nzjvirtual@gmail.com>
2025-03-25 14:08:25 +0100

    Agregar componente de inicio de sesión con diseño responsivo y funcionalidad para mostrar contraseña

--- src/app/components/auth/iniciarsesion/iniciarsesion.component.ts
+++ src/app/components/auth/iniciarsesion/iniciarsesion.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-iniciarsesion',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './iniciarsesion.component.html',
   styleUrl: './iniciarsesion.component.css'
 })
-export class IniciarsesionComponent {
+export class IniciarsesionComponent implements OnDestroy, OnInit {
+  // TODO MOSTRAR CONTRASEÑA
+  showPass = false;
 
+  togglePass(){
+    this.showPass = !this.showPass;
+  }
+
+
+  // TODO IMAGENES
+  imagenes = [
+    'https://i.pinimg.com/736x/24/1a/33/241a33e322eade5ed3f487b2ac9ea9f5.jpg',
+    'https://i.pinimg.com/236x/f8/1b/39/f81b39d7555259c63c23cca0ac0b635e.jpg',
+  ];
+
+  currentImgLng = this.imagenes[0];
+
+  private imgIn: any;
+  private curretnImgID = 0;
+
+
+  ngOnInit() {
+    this.empezar();
+  }
+
+  ngOnDestroy() {
+    if (this.imgIn) {
+      clearInterval(this.imgIn)
+    }
+  }
+
+  private empezar() {
+    this.imgIn = setInterval(() => {
+      this.curretnImgID = (this.curretnImgID + 1) % this.imagenes.length
+      this.currentImgLng = this.imagenes[this.curretnImgID]
+    }, 5000)
+  }
 }
