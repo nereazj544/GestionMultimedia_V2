41f45408665dc05ca9b8e6ef8131581ba9bf7ae2
nereazj544 <nzjvirtual@gmail.com>
2025-03-25 12:26:44 +0100

    Agregar componente de pie de página y mejorar el encabezado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HeaderComponent, HomeComponent, CommonModule],
+  imports: [RouterOutlet, RouterModule, HeaderComponent, HomeComponent, CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
