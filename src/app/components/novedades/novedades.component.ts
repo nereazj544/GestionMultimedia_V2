696e76357808b72f4e023112600606b55d28c92e
nereazj544 <nzjvirtual@gmail.com>
2025-03-25 19:54:20 +0100

    Agregar funcionalidad de búsqueda y filtros en el componente de novedades, incluyendo diseño mejorado y manejo de eventos

--- src/app/components/novedades/novedades.component.ts
+++ src/app/components/novedades/novedades.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-novedades',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './novedades.component.html',
   styleUrl: './novedades.component.css'
 })
 export class NovedadesComponent {
   isVideogameFilter: boolean = false;
   filtros = ['Libros', 'Peliculas', 'Series', 'Videojuegos'];
-  plataformas = ['PS', 'XBOX', 'SWITCH', 'PC']
+  plataformas = ['PS5', 'XBOX X|S', 'SWITCH', 'PC'];
+  searchText: string = '';
+  currentFilter: string = '';
 
+  constructor() { }
 
+  toggleFilter(filtro: string) {
+    if (filtro === 'Videojuegos') {
+      this.isVideogameFilter = !this.isVideogameFilter;
+    }
+    this.currentFilter = filtro;
+  }
+
+  filtrarPorPlataforma(plataforma: string) {
+    console.log('Filtrando por plataforma:', plataforma);
+  }
+
+  buscar(event: any) {
+    this.searchText = event.target.value;
+    console.log('Buscando:', this.searchText);
+  }
 }
