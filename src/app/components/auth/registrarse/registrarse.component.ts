ca22971eaa90e1e0f4f7c8fbcfb3cb9cb126aa74
nereazj544 <nzjvirtual@gmail.com>
2025-03-25 17:35:41 +0100

    Agregar nueva imagen al componente de registro

--- src/app/components/auth/registrarse/registrarse.component.ts
+++ src/app/components/auth/registrarse/registrarse.component.ts
@@ -20,7 +20,8 @@ export class RegistrarseComponent implements OnDestroy, OnInit {
     'https://i.pinimg.com/736x/24/1a/33/241a33e322eade5ed3f487b2ac9ea9f5.jpg',
     'https://i.pinimg.com/236x/f8/1b/39/f81b39d7555259c63c23cca0ac0b635e.jpg',
     'https://i.pinimg.com/736x/02/aa/6f/02aa6fc8f112bce0f0fcfd47b8940666.jpg',
-    'https://i.pinimg.com/736x/21/e4/ee/21e4eebb78a1febe8e8268a6c24056d8.jpg'
+    'https://i.pinimg.com/736x/21/e4/ee/21e4eebb78a1febe8e8268a6c24056d8.jpg',
+    'https://i.pinimg.com/736x/89/de/a2/89dea2e415fe3e3dd184269290e9dc46.jpg'
   ];
 
   currentImgRg = this.imagenes[0];
