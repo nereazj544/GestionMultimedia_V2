98b23c863aaacfc53591ed6a711247eb3a9bb792
nereazj544 <nzjvirtual@gmail.com>
2025-03-28 19:42:49 +0100

    Agregar componente 'Docs' y tooltip en el componente 'Caja', mejorando la navegación y la interfaz de usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,16 @@ import { ErrorComponent } from './components/error/error.component';
 import { IniciarsesionComponent } from './components/auth/iniciarsesion/iniciarsesion.component';
 import { RegistrarseComponent } from './components/auth/registrarse/registrarse.component';
 import { NovedadesComponent } from './components/novedades/novedades.component';
+import { DocsComponent } from './components/docs/docs.component';
+
+//* ===========================
+
 export const routes: Routes = [
 
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'novedades', component: NovedadesComponent },
+    { path: 'docs', component: DocsComponent },
 
     {path: 'iniciarsesion', component: IniciarsesionComponent},
     {path: 'registrarse', component: RegistrarseComponent},
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router, NavigationEnd } from '@angular/router';
-import e from 'express';
+
 
 @Component({
   selector: 'app-header',
